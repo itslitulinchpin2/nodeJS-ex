aa740530fb63412c9497374229715dfac03d1247
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-03-07 00:17:43 +0100

    check post method with postman

--- Node-server/json.js
+++ Node-server/json.js
@@ -12,22 +12,29 @@ const server = http.createServer((req,res)=>{
     console.log('incoming...');
 
     const url = req.url; //what?
+    console.log('url: ', url);
     const method = req.method; //action?
+    console.log('method: ', method);
 
     if (url==='/courses'){
+
         if(method==='GET'){
             res.writeHead(200, {'Content-Type':'application/json'})
-            res.end(JSON.stringify(courses)); //converts JavaScript objects into strings.
-        } else if (method ==='POST'){
+            res.end(JSON.stringify(courses)); //객체를 JSON으로
+        }
+        else if (method ==='POST'){
+
             const body = [];
+
             req.on('data', (chunk) => {
                 console.log(chunk);
                 body.push(chunk);
             })
 
             req.on('end', ()=>{
+
                 const bodyStr = Buffer.concat(body).toString()
-                const course = JSON.parse(bodyStr);
+                const course = JSON.parse(bodyStr); // JSON을 객체로
                 courses.push(course);
                 console.log(course);
 
